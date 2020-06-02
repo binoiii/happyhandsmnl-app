3b327e00b9eba7c6a613d8d5f448ee0855d993cd
binothird <albinoiii.cordova@gmail.com>
2020-06-02 23:50:18 +0800

    Started creation of Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,31 @@
 import React from "react";
+import SocialLinks from "./SocialLinks/SocialLinks";
 import "./Home.css";
 
-function Home(props) {
-  const socialLinks =
-    props.data &&
-    props.data.social.map((entry) => {
-      return (
-        <li key={entry.name}>
-          <a href={entry.url} target="_blank" rel="noopener noreferrer">
-            <i className={entry.className} />
-          </a>
-        </li>
-      );
-    });
+import personalized from "./img/personalized.png";
 
+function Home(props) {
   return (
-    <div id="home" className="Home__Wrap">
-      <h1>hh ...</h1>
-      <ul className="social">{socialLinks}</ul>
+    <div id="home" className="Home__cont">
+      <div className="HomeMain__cont">
+        <div className="HomeLeft__cont">
+          <div className="ecofriendly">eco - friendly</div>
+          <div className="personalized">personalized</div>
+          <div className="economical">economical</div>
+          <div className="makethe">make the</div>
+          <div className="socials">socials</div>
+        </div>
+        <div className="HomeRight__cont">
+          <div className="ecofriendly">eco - friendly</div>
+          <img src={personalized} className="personalized"></img>
+          <div className="economical">economical</div>
+          <div className="switch">switch</div>
+          <div className="socials">socials</div>
+        </div>
+      </div>
+      {/* <div className="SocialLinks_cont">
+        <SocialLinks props={props} />
+      </div> */}
     </div>
   );
 }
---
+++ src/Components/Home/SocialLinks/SocialLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./SocialLinks.css";
+
+const SocialLinks = ({ props }) => (
+  <div className="SocialLinks_cont">
+    social_links
+    <ul className="social">
+      {/* Social Links */}
+      {/* 
+      {props.data &&
+        props.data.social.map((entry) => (
+          <li key={entry.name}>
+            <a href={entry.url} target="_blank" rel="noopener noreferrer">
+              <i className={entry.className} />
+            </a>
+          </li>
+        ))} */}
+    </ul>
+  </div>
+);
+
+export default SocialLinks;
