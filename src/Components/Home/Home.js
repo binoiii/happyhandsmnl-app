a6997abd7f4cabb301d2313bcc39e914d773db72
binothird <albinoiii.cordova@gmail.com>
2020-06-15 23:58:55 +0800

    Update home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = (props) => {
         <div className="HhStatement">
           <div>MAKE THE SWITCH</div>
         </div>
-        <div className="HhSubStatement">
+        <div className="HhSubStatement__cont">
           <div>eco-friendly</div>
           <div className="HhSubStatement__inactive">&nbsp;.&nbsp;</div>
           <div>personalized</div>
@@ -20,37 +20,11 @@ const Home = (props) => {
           <a href="//">SHOP NOW</a>
         </div>
       </div>
-      <div className="SocialLinks_cont">
+      <div className="SocialLinks__cont">
         <SocialLinks props={props} />
       </div>
     </div>
   );
 };
-
-// function Home(props) {
-// return (
-//     <div id="home" className="Home__cont">
-//       <div className="HomeMain__cont">
-//         <div className="HomeLeft__cont">
-//           <div className="ecofriendly">eco - friendly</div>
-//           <div className="personalized">personalized</div>
-//           <div className="economical">economical</div>
-//           <div className="makethe">make the</div>
-//           <div className="socials"></div>
-//         </div>
-//         <div className="HomeRight__cont">
-//           <img src={personalized} className="ecofriendly"></img>
-//           <img src={personalized} className="personalized"></img>
-//           <img src={economical} className="economical"></img>
-//           <div className="switch">switch</div>
-//           <div className="socials"></div>
-//         </div>
-//       </div>
-//       <div className="SocialLinks_cont">
-//         <SocialLinks props={props} />
-//       </div>
-//     </div>
-//   );
-// }
 
 export default Home;
