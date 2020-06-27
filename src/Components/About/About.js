41e1145f05453339dcb21bfaeef9060cc9417f58
binothird <albinoiii.cordova@gmail.com>
2020-06-28 02:05:12 +0800

    Updated about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div id="about" className="About__cont">
       <div className="About__main">
-        <div className="About__title">about</div>
+        <div className="About__title">About</div>
         <div className="About__contents">
           <div>wide variety of souvenir items</div>
           <div>heart and happy hands crafted</div>
@@ -17,21 +17,21 @@ const About = () => {
       </div>
       <div className="MissionVision__cont">
         <div className="Mission__cont">
-          <div className="Mission__title">mission</div>
+          <div className="Mission__title">Mission</div>
           <div className="Mission_content">
             encourage people <br />
             to make the switch into <br />
             the green side to help <br />
-            our one and only planet
+            our one and only planet.
           </div>
         </div>
         <div className="Vision__cont">
-          <div className="Vision__title">vision</div>
+          <div className="Vision__title">Vision</div>
           <div className="Vision_content">
             spread eco-friendly <br />
             souvenirs with a <br />
             personalized touch <br />
-            globally
+            globally.
           </div>
         </div>
       </div>
@@ -39,52 +39,52 @@ const About = () => {
         <div className="Crafted__title">crafted with</div>
         <div className="Logo__cont">
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
           <div className="Logo__box">
-            <img src={logo1} />
+            <img src={logo1} alt="test" />
           </div>
         </div>
       </div>
