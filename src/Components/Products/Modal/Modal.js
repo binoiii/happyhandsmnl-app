06b262ea63a77bda6d190459d59d485f3892f9f9
binothird <albinoiii.cordova@gmail.com>
2020-07-23 18:27:59 +0800

    Enhanced modal

--- src/Components/Products/Modal/Modal.js
+++ src/Components/Products/Modal/Modal.js
@@ -1,16 +1,19 @@
-import React from "react";
-import ImageHolder from "./ImageHolder/ImageHolder";
+import React, { Suspense, lazy } from "react";
 import "./Modal.css";
+
+const ImageHolder = lazy(() => import("./ImageHolder/ImageHolder"));
 
 const Modal = ({ exitModal, prodImages, prodName }) => {
   return (
     <div className="Modal__cont" onClick={exitModal}>
       <div className="Image__cont">
-        <ImageHolder
-          exitModal={exitModal}
-          prodImages={prodImages}
-          prodName={prodName}
-        />
+        <Suspense fallback={<h1>Loading</h1>}>
+          <ImageHolder
+            exitModal={exitModal}
+            prodImages={prodImages}
+            prodName={prodName}
+          />
+        </Suspense>
       </div>
     </div>
   );
