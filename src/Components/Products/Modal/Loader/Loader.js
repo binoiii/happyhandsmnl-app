1f68b1c94224e98dafae5e7bf33fa35d7250ece5
binothird <albinoiii.cordova@gmail.com>
2020-07-23 20:22:29 +0800

    Added loader

---
+++ src/Components/Products/Modal/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div class="loadingio-spinner-rolling-tlcifmpvh4c">
+      <div class="ldio-gwnp1ah044p">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/Components/Products/Modal/Modal.js
+++ src/Components/Products/Modal/Modal.js
@@ -1,5 +1,8 @@
 import React, { Suspense, lazy } from "react";
+
 import "./Modal.css";
+
+import Loader from "./Loader/Loader";
 
 const ImageHolder = lazy(() => import("./ImageHolder/ImageHolder"));
 
@@ -7,7 +10,7 @@ const Modal = ({ exitModal, prodImages, prodName }) => {
   return (
     <div className="Modal__cont" onClick={exitModal}>
       <div className="Image__cont">
-        <Suspense fallback={<h1>Loading</h1>}>
+        <Suspense fallback={<Loader />}>
           <ImageHolder
             exitModal={exitModal}
             prodImages={prodImages}
