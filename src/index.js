2e7bd533472a36d0217b1e259f50eecdad0d9ffa
binothird <albinoiii.cordova@gmail.com>
2020-09-13 14:28:24 +0800

    Added new fonts

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "./Fonts/bernadette.ttf";
-import "./Fonts/baybayin.ttf";
-
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
